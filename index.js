89aa4e9358b54937a77b6a2785f9abd3dc50ba1f
iepoch <staclem@live.com>
2018-11-29 21:10:30 -0700

    Added push delete and post

--- index.js
+++ index.js
@@ -6,38 +6,119 @@ const db = require("./data/db");
 const server = express();
 const PORT = 4000;
 
-server.get("/api/users", (request, response) => {
+server.use(express.json())
+
+server.get("/api/users", (req, res) => {
+    server.disable('etag')
 db.find()
     .then(users => {
-    response.json(users);
+    res.json(users);
     })
     .catch(err => {
-        response
+        res
             .status(500)
             .json({ message: "failed to get users" });
     });
 });
 
-server.get("/api/users/:id", (request, response) => {
-const { id } = request.params;
+server.get("/api/users/:id", (req, res) => {
+const { id } = req.params;
 db.findById(id)
     .then(user => {
         if (user) {
-            response.json(user);
+            res.json(user);
         }
         else {
-            response
+            res
                 .status(404)
                 .json({message: 'user does not exist'})
     }
     })
     .catch(err => {
-        response
+        res
             .status(404)
             .json({ message: "failed to get users" })
     }
     );
 });
+
+server.post("/api/users", (req, res) => {
+    const user = req.body;
+    console.log('user in body', user)
+
+    if (user.name && user.bio) {
+    db.insert(user)
+        .then(response => {
+
+                db.findById(response.id).then(user => {
+
+                    res.status(201).json(user)
+                })
+            })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: "failed to get users" })
+        })
+} else {
+        res.status(400).json({message: "missing name or bio"})
+}
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    db.remove(id)
+        .then(count => {
+            if (count) {
+                //something been deleted
+                //send back user (this is a cheater way)
+                res.json({
+                    message: "Successfully Deleted"})
+            } else {
+                //If there is no id or already deleted
+                res.status(404)
+                    .json({message:"invalid id"})
+            }
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: "failed to delete user" })
+        })
+})
+
+
+server.put('/api/users/:id', (req, res) => {
+    const user = req.body;
+    const { id } = req.params;
+
+    if (user.name && user.bio) {
+        db.update(id, user)
+            .then(count => {
+                if (count) {
+                    db.findById(id).then(user => {
+                        res
+                        .json(user)
+
+                    })
+                } else {
+                    res.status(404)
+                    .json({message:"invalid id"})
+                }
+            })
+            .catch(err => {
+                res
+                    .status(500)
+                    .json({ message: "failed to get users" })
+            })
+    }
+    else {
+        res.status(400)
+            .json({message:"Missing your name or bio"})
+    }
+
+})
+
 
 server.listen(PORT, () => {
 console.log(`server is up and running ${PORT}`);
